873466e1852de94b1117f82084bdc22571ee1550
newborn1000 <daniel@newbornwebs.net>
2013-07-26 21:44:32 +0100

    Update StreakRecordDao.java

--- src/main/java/org/simiancage/DeathTpPlus/death/persistence/StreakRecordDao.java
+++ src/main/java/org/simiancage/DeathTpPlus/death/persistence/StreakRecordDao.java
@@ -57,7 +57,7 @@ public class StreakRecordDao implements Runnable {
         }
         load();
 
-        Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, this, SAVE_DELAY, SAVE_PERIOD);
+        Bukkit.getScheduler().runTaskTimerAsynchronously(JavaPlugin, this, SAVE_DELAY, SAVE_PERIOD);
     }
 
     private void load() {
